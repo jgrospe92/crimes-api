3990f2ba4427e3b17a825346557c1a76e5f199e7
Md Saqliyan Islam <82405606+SaqSaq815@users.noreply.github.com>
2023-03-14 20:09:30 -0400

    worked on crime-api database file

--- database/crimes-api.sql
+++ database/crimes-api.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 13, 2023 at 08:31 PM
+-- Generation Time: Mar 15, 2023 at 01:06 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -62,6 +62,35 @@ CREATE TABLE `cases_victims` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `courts`
+--
+
+CREATE TABLE `courts` (
+  `court_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `date` date NOT NULL,
+  `time` date NOT NULL,
+  `address_id` int(11) NOT NULL,
+  `judge_id` int(11) NOT NULL,
+  `verdict_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `court_addresses`
+--
+
+CREATE TABLE `court_addresses` (
+  `address_id` int(11) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `street` varchar(100) NOT NULL,
+  `building_#` varchar(7) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `crime_scenes`
 --
 
@@ -76,6 +105,20 @@ CREATE TABLE `crime_scenes` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `defendants`
+--
+
+CREATE TABLE `defendants` (
+  `defendant_id` int(11) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `age` int(4) NOT NULL,
+  `specialization` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `investigators`
 --
 
@@ -85,6 +128,47 @@ CREATE TABLE `investigators` (
   `first_name` varchar(100) NOT NULL,
   `last_name` varchar(100) NOT NULL,
   `rank` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `judges`
+--
+
+CREATE TABLE `judges` (
+  `judge_id` int(11) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `age` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `offenders`
+--
+
+CREATE TABLE `offenders` (
+  `offender_id` int(11) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `age` int(4) NOT NULL,
+  `marital_status` enum('single','married') NOT NULL,
+  `arrest_date` date NOT NULL,
+  `arrest_timestamp` date NOT NULL,
+  `defendant_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `offender_details`
+--
+
+CREATE TABLE `offender_details` (
+  `offender_id` int(11) NOT NULL,
+  `case_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -112,6 +196,19 @@ CREATE TABLE `prosecutors` (
   `last_name` varchar(100) NOT NULL,
   `age` int(7) DEFAULT NULL,
   `specialization` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `verdicts`
+--
+
+CREATE TABLE `verdicts` (
+  `verdict_id` int(11) NOT NULL,
+  `name` int(100) NOT NULL,
+  `description` text NOT NULL,
+  `fine` int(7) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -155,16 +252,57 @@ ALTER TABLE `cases_victims`
   ADD KEY `cases_victims_composite` (`case_id`,`victim_id`);
 
 --
+-- Indexes for table `courts`
+--
+ALTER TABLE `courts`
+  ADD PRIMARY KEY (`court_id`),
+  ADD KEY `courts_judge_id_fk` (`judge_id`),
+  ADD KEY `courts_address_if_fk` (`address_id`),
+  ADD KEY `courts_verdict_id_fk` (`verdict_id`);
+
+--
+-- Indexes for table `court_addresses`
+--
+ALTER TABLE `court_addresses`
+  ADD PRIMARY KEY (`address_id`);
+
+--
 -- Indexes for table `crime_scenes`
 --
 ALTER TABLE `crime_scenes`
   ADD PRIMARY KEY (`crime_sceneID`);
 
 --
+-- Indexes for table `defendants`
+--
+ALTER TABLE `defendants`
+  ADD PRIMARY KEY (`defendant_id`);
+
+--
 -- Indexes for table `investigators`
 --
 ALTER TABLE `investigators`
   ADD PRIMARY KEY (`investigator_id`);
+
+--
+-- Indexes for table `judges`
+--
+ALTER TABLE `judges`
+  ADD PRIMARY KEY (`judge_id`);
+
+--
+-- Indexes for table `offenders`
+--
+ALTER TABLE `offenders`
+  ADD PRIMARY KEY (`offender_id`),
+  ADD KEY `offenders_defendant_id_fk` (`defendant_id`);
+
+--
+-- Indexes for table `offender_details`
+--
+ALTER TABLE `offender_details`
+  ADD PRIMARY KEY (`offender_id`),
+  ADD KEY `offender_detail_case_id_fk` (`case_id`);
 
 --
 -- Indexes for table `offenses`
@@ -177,6 +315,12 @@ ALTER TABLE `offenses`
 --
 ALTER TABLE `prosecutors`
   ADD PRIMARY KEY (`prosecutor_id`);
+
+--
+-- Indexes for table `verdicts`
+--
+ALTER TABLE `verdicts`
+  ADD PRIMARY KEY (`verdict_id`);
 
 --
 -- Indexes for table `victims`
@@ -196,16 +340,46 @@ ALTER TABLE `cases`
   MODIFY `case_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `courts`
+--
+ALTER TABLE `courts`
+  MODIFY `court_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `court_addresses`
+--
+ALTER TABLE `court_addresses`
+  MODIFY `address_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `crime_scenes`
 --
 ALTER TABLE `crime_scenes`
   MODIFY `crime_sceneID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `defendants`
+--
+ALTER TABLE `defendants`
+  MODIFY `defendant_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `investigators`
 --
 ALTER TABLE `investigators`
   MODIFY `investigator_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `judges`
+--
+ALTER TABLE `judges`
+  MODIFY `judge_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `offenders`
+--
+ALTER TABLE `offenders`
+  MODIFY `offender_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `offenses`
@@ -218,6 +392,12 @@ ALTER TABLE `offenses`
 --
 ALTER TABLE `prosecutors`
   MODIFY `prosecutor_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `verdicts`
+--
+ALTER TABLE `verdicts`
+  MODIFY `verdict_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `victims`
@@ -235,6 +415,28 @@ ALTER TABLE `victims`
 ALTER TABLE `cases`
   ADD CONSTRAINT `cases_crime_scenes_fk` FOREIGN KEY (`crime_sceneID`) REFERENCES `crime_scenes` (`crime_sceneID`) ON DELETE CASCADE,
   ADD CONSTRAINT `cases_investigator_id_fk` FOREIGN KEY (`investigator_id`) REFERENCES `investigators` (`investigator_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `courts`
+--
+ALTER TABLE `courts`
+  ADD CONSTRAINT `courts_address_if_fk` FOREIGN KEY (`address_id`) REFERENCES `court_addresses` (`address_id`),
+  ADD CONSTRAINT `courts_case_id_fk` FOREIGN KEY (`court_id`) REFERENCES `cases` (`court_id`),
+  ADD CONSTRAINT `courts_judge_id_fk` FOREIGN KEY (`judge_id`) REFERENCES `judges` (`judge_id`),
+  ADD CONSTRAINT `courts_verdict_id_fk` FOREIGN KEY (`verdict_id`) REFERENCES `verdicts` (`verdict_id`);
+
+--
+-- Constraints for table `offenders`
+--
+ALTER TABLE `offenders`
+  ADD CONSTRAINT `offenders_defendant_id_fk` FOREIGN KEY (`defendant_id`) REFERENCES `defendants` (`defendant_id`);
+
+--
+-- Constraints for table `offender_details`
+--
+ALTER TABLE `offender_details`
+  ADD CONSTRAINT `offender_detail_case_id_fk` FOREIGN KEY (`case_id`) REFERENCES `cases` (`case_id`),
+  ADD CONSTRAINT `offender_details_offender_id_fk` FOREIGN KEY (`offender_id`) REFERENCES `offenders` (`offender_id`);
 
 --
 -- Constraints for table `victims`
