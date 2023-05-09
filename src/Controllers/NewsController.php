6f2438096617f53a8124081b0d78f8f18af21703
Md Saqliyan Islam <82405606+SaqSaq815@users.noreply.github.com>
2023-05-09 13:24:09 -0400

    update composite

--- src/Controllers/NewsController.php
+++ src/Controllers/NewsController.php
@@ -6,18 +6,22 @@ class NewsController extends WebServiceInvoker
 {
     public function getNews()
     {
+
         $news_URI = 'https://saurav.tech/NewsAPI/';
         $data = $this->invokeUri($news_URI,'everything/cnn.json');
 
         //var_dump($data);exit;
+        $counter = 0;
         $news_data = [];
         foreach ($data->articles as $key => $article) {
             $news_data[$key]['author'] = $article->author;
             $news_data[$key]['title'] = $article->title;
             $news_data[$key]['description'] = $article->description;
             $news_data[$key]['url'] = $article->url;
-
-
+            $counter++ ;
+            if($counter == 15){
+                break;
+            }
         }
         //var_dump($news_data);exit;
         return $news_data;
