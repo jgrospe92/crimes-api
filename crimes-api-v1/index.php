2175708699adafc6bad89db7a7b056e7c50b8022
Md Saqliyan Islam <82405606+SaqSaq815@users.noreply.github.com>
2023-05-16 10:46:34 -0400

    Add files via upload

---
+++ crimes-api-v1/index.php
@@ -0,0 +1,81 @@
+<?php
+
+
+use Slim\Factory\AppFactory;
+use Slim\Factory\ServerRequestCreatorFactory;
+use Vanier\Api\exceptions\HttpErrorHandler;
+use Vanier\Api\Helpers\JWTManager;
+use Vanier\Api\middleware\ContentNegotiationMiddleware;
+use Vanier\Api\middleware\LoggerMiddleware;
+use Vanier\Api\Middleware\JWTAuthMiddleware;
+
+
+// iff config.env exists doesn't exists, create it
+$file = 'config.env';
+if (!is_file($file)) {
+    $contents = 'SECRET_KEY="ThisIsATopSecretKey"';
+    file_put_contents($file, $contents);
+}
+
+
+define('APP_BASE_DIR', __DIR__);
+// IMPORTANT: This file must be added to your .ignore file. 
+define('APP_ENV_CONFIG', 'config.env');
+define('APP_JWT_TOKEN_KEY', 'APP_JWT_TOKEN');
+
+require __DIR__ . '/vendor/autoload.php';
+// Include the file that contains the application's global configuration settings,
+// database credentials, etc.
+require_once __DIR__ . '/src/Config/app_config.php';
+
+//--Step 1) Instantiate a Slim app.
+$app = AppFactory::create();
+$routeCollector = $app->getRouteCollector();
+// add callable
+$callableResolver = $app->getCallableResolver();
+$responseFactory = $app->getResponseFactory();
+// add server request
+$serverRequestCreator = ServerRequestCreatorFactory::create();
+$request = $serverRequestCreator->createServerRequestFromGlobals();
+$errorHandler = new HttpErrorHandler($callableResolver, $responseFactory);
+
+// Parse json, form data and xml, first stack
+$app->addBodyParsingMiddleware();
+
+//-- Add the routing , second stack
+//-- Add the routing and body parsing middleware, second stack
+$app->addRoutingMiddleware();
+
+// TEST
+
+// logger middleware, third stack
+$logger = new LoggerMiddleware();
+$app->add($logger);
+
+// AA middleware, fourth stack
+$jwt_secret = JWTManager::getSecretKey();
+$app->add(new JWTAuthMiddleware());
+
+// Must be added before last. Parse json, form data and xml
+$app->addBodyParsingMiddleware();
+//-- Add error handling middleware.
+// NOTE: the error middleware MUST be added last.
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+$errorMiddleware->setDefaultErrorHandler($errorHandler);
+$errorMiddleware->getDefaultErrorHandler()->forceContentType(APP_MEDIA_TYPE_JSON);
+
+// content negotiation middleware, end of stack
+$app->add(new ContentNegotiationMiddleware());
+
+//---
+// You also need to change it in .htaccess
+$app->setBasePath("/crimes-api-v1");
+
+// Here we include the file that contains the application routes. 
+// NOTE: your routes must be managed in the api_routes.php file.
+require_once __DIR__ . '/src/Routes/api_routes.php';
+
+// This is a middleware that should be disabled/enabled later. 
+//$app->add($beforeMiddleware);
+// Run the app.
+$app->run();
